e51549ff66f839328a634d803a7031528343b386
Karan Bhalla <karanbhalla204@gmail.com>
2024-11-09 15:33:47 -0600

    Added basic order by month graph

--- _pages/another_page.py
+++ _pages/another_page.py
@@ -1,3 +1,23 @@
 import streamlit as st
+from data.utils import load_data
+from analytics.orders import *
 
-st.title("Another Page")
+
+st.title("Data Hub")
+
+data = load_data()
+
+st.write(data['Sent Date'].dt.month_name().value_counts())
+
+months = st.multiselect(
+    "Please select months", 
+    data["Sent Date"].dt.month_name().unique(),
+    key = "month_input"
+)
+
+
+monthOrders = orders_in_month(data, months)
+st.line_chart(monthOrders)
+
+
+    
--- _pages/home_page.py
+++ _pages/home_page.py
@@ -1,3 +1,6 @@
 import streamlit as st
+from data.utils import load_data
 
 st.title("Home Page")
+
+data = load_data()
