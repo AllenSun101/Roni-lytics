0b56c4df9bcd993113150e681e7983b06515ace8
AllenSun101 <allensun21527@gmail.com>
2024-11-09 20:11:55 -0600

    Add logo

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 from data.utils import load_data
 
+st.logo("logo.png", size="large")
+
 navigation = get_nav_from_toml()
+
 pg = st.navigation(navigation)
 pg.run()
 
