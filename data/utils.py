24425888cb58fdfd7f13b545eb39c68958d998fa
AllenSun101 <allensun21527@gmail.com>
2024-11-09 18:56:15 -0600

    Add progress

--- data/utils.py
+++ data/utils.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("https://raw.githubusercontent.com/Aran203/ronis-viz-td-2024/refs/heads/main/data/data_full.csv", parse_dates = ["Sent Date"])
+    df = pd.read_csv("https://raw.githubusercontent.com/Aran203/ronis-viz-td-2024/refs/heads/main/data/data_processed.csv", parse_dates = ["time"])
     return df
