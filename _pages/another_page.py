ced0dd9625dfd6b7cb29661bbecaa893625dbb36
Karan Bhalla <karanbhalla204@gmail.com>
2024-11-09 21:14:06 -0600

    Added order stats

--- _pages/another_page.py
+++ _pages/another_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import altair as alt
 import pandas as pd
-from datetime import timedelta
+from datetime import *
 
 from data.utils import load_data
 from analytics.orders import *
@@ -10,21 +11,25 @@ data = load_data()
 st.title("Orders Hub")
 
 
+# Getting inputs through a container
 container = st.container()
 
 input = container.container()
 left, right = input.columns(2)
 
-start_date = left.date_input("Choose a start date:", data['Sent Date'].min(), 
-                             min_value = data["Sent Date"].min(), 
-                             max_value = data["Sent Date"].max(),
+start_date = left.date_input("Choose a start date:", data['time'].min(), 
+                             min_value = data["time"].min(), 
+                             max_value = data["time"].max(),
                              format = "MM/DD/YYYY",
                              key = "start_date"
                             )
 
-start_time = right.time_input("Choose a start time:", step = timedelta(minutes = 10),
+start_time = right.time_input("Choose a start time:", time(11, 0),
+                              step = timedelta(minutes = 30),
                               key = "start_time"
                               )
+
+start_datetime = datetime.combine(start_date, start_time)
 
 
 output = container.container()
@@ -32,17 +37,77 @@ left1, right1 = output.columns(2)
 
 end_date = left1.date_input("Choose a start date:", start_date, 
                              min_value = start_date, 
-                             max_value = data["Sent Date"].max(),
+                             max_value = data["time"].max(),
                              format = "MM/DD/YYYY",
                              key = "end_date"
                             )
 
-end_time = right1.time_input("Choose a start time:", start_time ,
-                              step = timedelta(minutes = 10),
+end_time = right1.time_input("Choose a end time:", time(22, 59) ,
+                              step = timedelta(minutes = 30),
                               key = "end_time"
                               )
 
+end_datetime = datetime.combine(end_date, end_time)
 
+
+if (start_datetime > end_datetime):
+    st.write(":red[Error: Start time must be before end time]")
+else:
+    test = get_num_orders(data, start_datetime, end_datetime)
+
+    if (end_datetime - start_datetime > timedelta(days = 69)):
+        chart = alt.Chart(test).mark_line().encode(
+            x=alt.X('Time:T', title='Date', axis = alt.Axis(format='%B', tickCount = 4)),  
+            y=alt.Y('Order Count:Q', title='Order Count')  
+        )
+    elif (end_datetime - start_datetime > timedelta(days = 6)):
+        chart = alt.Chart(test).mark_line().encode(
+            x=alt.X('Time:T', title='Date', axis = alt.Axis(format='%b %d')),  
+            y=alt.Y('Order Count:Q', title='Order Count')  
+        )
+    elif (end_datetime - start_datetime > timedelta(days = 3)):
+        chart = alt.Chart(test).mark_line().encode(
+            x=alt.X('Time:T', title='Date', axis = alt.Axis(format='%b %d', tickCount = 3)),  
+            y=alt.Y('Order Count:Q', title='Order Count')  
+        )
+    else:
+        chart = alt.Chart(test).mark_line().encode(
+            x=alt.X('time:T', title = 'Date', axis = alt.Axis(format='%b-%d-%I-%p', tickCount = 14)),  
+            y=alt.Y('Order Count:Q', title='Order Count')  
+        )
+
+
+    container.write("")
+    container.write("")
+    container.altair_chart(chart, use_container_width=True)
+
+
+    
+
+
+
+
+
+
+# container = st.container()
+# container.write("Choose period over which you want to see order count")
+
+# month = st.selectbox(
+#     "Please select month to look at stats for", 
+#     data["Sent Date"].dropna().dt.month_name().unique(),
+#     key = "month_input",
+#     index = 1
+# )
+
+# st.write("")
+# st.write("")
+# monthOrders = orders_in_month(data, month)
+# st.line_chart(monthOrders, x_label = month, y_label = "Orders")
+
+
+# dayOrders = orders_in_day(data, month, 23)
+# # print(dayOrders)
+# st.line_chart(dayOrders, x_label = f'Hour', y_label = "Orders")
 
 
     
--- analytics/orders.py
+++ analytics/orders.py
@@ -1,10 +1,56 @@
+from datetime import timedelta
+import pandas as pd
 
-def orders_in_month(df, months):
-    dfMonth = df.loc[df['Sent Date'].dt.month_name().isin(months)]
+def get_num_orders(df, start_time, end_time):
+    data = df.loc[(df['time'] >= start_time) & (df['time'] <= end_time)]
+    data = data.copy()
 
-    dfMonth['day'] = dfMonth['Sent Date'].dt.day
-    days = dfMonth.groupby(dfMonth["day"])['Order ID'].nunique()
+    if (end_time - start_time > timedelta(days = 69)):
+        data["Time"] = data['time'].dt.strftime('%m')  
+        out = data.groupby("Time")['orderID'].nunique()
+        out = out.to_frame(name = "Order Count").reset_index()
+        out = out.rename(columns = {"orderID": "Orders"})
+    elif ((start_time.month != end_time.month) or (end_time - start_time > timedelta(days = 3))):
+        data["Time"] = data['time'].dt.strftime('%m-%d')  
+        out = data.groupby("Time")['orderID'].nunique()
+        out = out.to_frame(name = "Order Count").reset_index()
+        out = out.rename(columns = {"orderID": "Orders"})
 
-    return days.to_frame()
+    else:
+        out = data.groupby(pd.Grouper(key='time', freq='1h'))['orderID'].nunique()
+        out = out.to_frame(name = "Order Count").reset_index()
+        out = out.rename(columns = {"orderID": "Orders"})
+                
+        
+    return out
+
+
+
+
+# def orders_in_month(df, month):
+#     dfMonth = df.loc[df['Sent Date'].dt.month_name() == month]
+#     dfMonth = dfMonth.copy()
+
+#     dfMonth[f'{month}'] = dfMonth['Sent Date'].dt.day
+#     days = dfMonth.groupby(dfMonth[f'{month}'])['Order ID'].nunique()
+
+#     days = days.to_frame()
+#     days = days.rename(columns = {"Order ID": "Orders"})
+
+#     return days
+
+# def orders_in_day(df, month, day):
+#     data = df.loc[(df['Sent Date'].dt.month_name() == month) & (df['Sent Date'].dt.day == day)]
+#     data = data.copy()
+
+#     data['Hour of Day'] = data['Sent Date'].dt.hour
+#     hours = data.groupby(data['Hour of Day'])['Order ID'].nunique()
+
+#     hours = hours.to_frame()
+#     hours = hours.rename(columns = {"Order ID": "Orders"})
+
+#     return hours
+
+
 
 
